f644cc1074101415895fbf7516e2dca45359e524
xniqt <xnnie.wrld@gmail.com>
2026-06-09 04:50:56 +0200

    Updated leaderboard

--- src/data/factions.js
+++ src/data/factions.js
@@ -5,7 +5,7 @@ export const factionsData = [
     members: ["ultragaminggamer", "KotaGG", "krittanica", "pendulesteak"], 
     completedVol1: ["hq", "tame", "banner", "rod", "bee", "redstone", "heart", "ender", "pumpkin", "breath", "farm", "map", "creeper", "egg", "horse", "apple", "mace", "armor", "beacons", "axolotl"],
     completedVol2: ["terracotta", "froglight", "egg_cake", "templates", "pink_armor", "hive_flowers", "goat_horn", "silence", "vex_trim", "ghast_cage", "conduit", "disc_5", "sherds", "spore", "sponge", "smelter", "god_armor", "map_5x5", "mooshroom", "monument", "end_heads", "cart_hub", "totems", "emerald_pyramid", "zoo", "chunk_loader", "amethyst_roof"],
-    completedVol3: ["sorting_system", "tame_trio", "sorting_system", "diamond_horses", "all_signs", "raw_ore_blocks"] 
+    completedVol3: ["sorting_system", "tame_trio", "sorting_system", "diamond_horses", "all_signs", "raw_ore_blocks", "nether_highway"] 
   },
   { 
     name: "boo", 
