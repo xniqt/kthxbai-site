c8fd1616114e88ac9052c5dcd1368328c60d422f
xniqt <xnnie.wrld@gmail.com>
2026-06-15 22:14:42 +0200

    Updated leaderboard

--- src/data/factions.js
+++ src/data/factions.js
@@ -19,6 +19,6 @@ export const factionsData = [
     members: ["Thunderstorm24", "Fet0921", "pantanaisu", "_______________s"],
     completedVol1: ["hq", "tame", "banner", "rod", "bee", "redstone", "heart", "ender", "pumpkin", "breath", "farm", "map", "creeper", "egg", "horse", "apple", "mace", "armor", "beacons", "axolotl"],
     completedVol2: ["terracotta", "froglight", "egg_cake", "templates", "pink_armor", "hive_flowers", "goat_horn", "silence", "vex_trim", "ghast_cage", "conduit", "disc_5", "sherds", "spore", "sponge", "smelter", "god_armor", "map_5x5", "mooshroom", "monument", "end_heads", "cart_hub", "totems", "advancement", "emerald_pyramid", "zoo", "chunk_loader", "amethyst_roof"],
-    completedVol3: ["gamba", "shopping", "charged_creeper", "ocean_dome", "shulker_chest", "all_signs", "sorting_system", "raw_ore_blocks"]
+    completedVol3: ["gamba", "shopping", "charged_creeper", "ocean_dome", "shulker_chest", "all_signs", "sorting_system", "raw_ore_blocks", "all_banners"]
   },
 ];
